af7ebe4ff872b1e2693428551175537dc0d35bcb
gghotted <gghotted2@naver.com>
2022-03-17 14:15:19 +0900

    DR-9 url testcase 작성

--- django_project/post/tests.py
+++ django_project/post/tests.py
@@ -1,3 +1,10 @@
+from common.test_utils import LoggedInTestCase
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PostCreateAPITestCase(LoggedInTestCase):
+    urlname = 'post:post-list'
+
+    def test_url(self):
+        self.assertEqual(reverse(self.urlname), '/post/')
